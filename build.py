e72752db04450ce73242d3b974bc004233da9d3b
nixen420 <nikhil.singh4008@gmail.com>
2017-06-21 19:33:08 +0000

    Done

--- build.py
+++ build.py
@@ -1,25 +1,15 @@
 import pandas as pd
 
-
 def add(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return (ds1 + ds2)
 
 
 def subtract(ds1, ds2):
-    """
-    Enter your code here
-    """
-
+    return (ds1 - ds2)
 
 def multiply(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return (ds1 * ds2)
 
 
 def divide(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return ds1 / ds2
