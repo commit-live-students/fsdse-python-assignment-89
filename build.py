6ce933e407540570fc3bf475de2d128bfdd7ef79
adinathauti <adinathauti@gmail.com>
2017-06-22 09:31:22 +0000

    Done

--- build.py
+++ build.py
@@ -1,25 +1,29 @@
 import pandas as pd
 
+ds_o = pd.Series()
 
 def add(ds1, ds2):
-    """
-    Enter your code here
-    """
-
+    ds_o = ds1+ds2
+    return ds_o
 
 def subtract(ds1, ds2):
-    """
-    Enter your code here
-    """
+    ds_o = ds1-ds2
+    return ds_o
 
 
 def multiply(ds1, ds2):
-    """
-    Enter your code here
-    """
+    ds_o = ds1*ds2
+    return ds_o
 
 
 def divide(ds1, ds2):
-    """
-    Enter your code here
-    """
+    ds_o = ds1/ds2
+    return ds_o
+
+dsa = pd.Series([1,2,3,4])
+dsb = pd.Series([4,5,6,7])
+
+add(dsa,dsb)
+subtract(dsb,dsa)
+multiply(dsa,dsb)
+divide(dsa,dsb)
