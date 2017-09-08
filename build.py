bd0cb35e0fc4d40c1a474c7f30a68545651787f1
rohanhg91 <rohanhgupta91@gmail.com>
2017-09-08 16:03:46 +0000

    Done

--- build.py
+++ build.py
@@ -1,25 +1,23 @@
 import pandas as pd
 
-
 def add(ds1, ds2):
-    """
-    Enter your code here
-    """
-
+    ad = pd.Series.add(ds1,ds2)
+    return ad
 
 def subtract(ds1, ds2):
-    """
-    Enter your code here
-    """
-
+    s = pd.Series.subtract(ds1,ds2)
+    return s
 
 def multiply(ds1, ds2):
-    """
-    Enter your code here
-    """
-
+    mul = pd.Series.multiply(ds1,ds2)
+    return mul
 
 def divide(ds1, ds2):
-    """
-    Enter your code here
-    """
+    div = pd.Series.divide(ds1,ds2)
+    return div
+
+a = [1,3,5,7]
+b = [2,4,6,8]
+a1 = pd.Series(a)
+b1 = pd.Series(b)
+print add(b1,a1)
