e99973c96137f05060bc1c8c01f50eb577c2d4ec
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-09-20 14:33:48 +0000

    Done

--- build.py
+++ build.py
@@ -5,21 +5,22 @@ def add(ds1, ds2):
     """
     Enter your code here
     """
-
+    return ds1 + ds2
 
 def subtract(ds1, ds2):
     """
     Enter your code here
     """
-
+    return ds1 - ds2
 
 def multiply(ds1, ds2):
     """
     Enter your code here
     """
-
+    return ds1 * ds2
 
 def divide(ds1, ds2):
     """
     Enter your code here
     """
+    return ds1 / ds2
